731c6d3919218fe7b117f93cce803aaf25312354
Sudeep4686 <sudeephipparge@gmail.com>
2023-08-10 08:19:47 +0530

    bcrypting password

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express')
-const path = require('path')
+const path = require('path');
+const bodyparser = require('body-parser');
+const controller = require('./controllers/login')
+const sequelize = require('./util/database');
+const axios = require('axios');
+
+const User = require('./models/user');
+
 const app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
 
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','signup.html'));
 })
+
+app.post('/submit-form',controller.insertData);
+
+sequelize.sync({force:false})
+.then(()=>{
+    console.log("details synced with database")
+})
+.catch((err)=>{
+    console.log("details could not be synced with database")
+})
+
 app.listen(2200);
---
+++ controllers/login.js
@@ -0,0 +1,72 @@
+const bodyparser = require('body-parser');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+async function insertData(req,res){
+    console.log(req.body, "Data Posting");
+    if (!req.body.name || !req.body.email ||!req.body.mobile || !req.body.password){
+        console.log("error");
+        return res.status(400).send({
+            message:"please fill all the details"
+        })
+    }
+    // const obj = {
+    //     name:req.body.name,
+    //     email:req.body.email,
+    //     mobile:req.body.mobile,
+    //     password:req.body.password
+    // }
+    try{
+        const {name,email,mobile,password } = req.body;
+        console.log("recieved data: ",name ,email,mobile,password);
+        const existingUser = await User.findOne({where : {email}});
+
+        if(existingUser){
+            console.log("User already Exists");
+            return res.status(409).send({message : "User already exists" });
+        }
+
+        const saltrounds = 10;
+        bcrypt.hash(password,saltrounds,async(err,hash)=>{
+            const user = await User.create({name,email,mobile,password:hash});
+            console.log("new user Created : ", user);
+            return res.status(201).send({
+            message:"User created successfully",
+            alert : "User has been created"
+        });
+        })    
+    }catch(err){
+        console.log("error:", err);
+        return res.status(500).send({message:"Internal server Error, check for the error"});
+    }
+}
+
+
+async function login(req,res){
+    const {email,password} = req.body;
+    console.log("recieved login data: ",email);
+    try{    
+        const user = await User.findOne({where:{email}});
+
+        if(user){
+            bcrypt.compare(password,user.password,(err,result)=>{
+                if(err){
+                    throw new Error("Something went wrong")
+                }
+                    if(result===true){
+                        res.status(200).json({success : true, message:"User Logged in successfully",token: generateToken(user.id,user.ispremiumuser)});
+                    }else{
+                        return res.status(400).json({success:false,message:"Incorrect password"})
+                    }
+            })
+        }else{
+            return res.status(404).send({success:false,message:"User doesnot exist"})
+        }
+
+    }catch(error){
+        console.log("error : ", error);
+        return res.status(500).send({message:"Internal Server Error"});
+    }
+}
+
+module.exports={insertData};
